355c1120979127e8a2ca393522c456b6c9618787
Aleksi <aleksi.martikainen@metropolia.fi>
2019-07-07 16:35:50 +0300

    aw

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-
+﻿
 
 var cards = [];
 
@@ -16,7 +16,7 @@ function start() {
 
     cards.push(deck[0], deck[1], deck[2]);
 
-    showCard(cards[0], "Player 1 card");
+    showCard(cards[0], "Player1card");
 }
 
 function shuffle(array) {
@@ -41,44 +41,102 @@ function shuffle(array) {
 
 function showCard(card, text) {
 
-    var paska = document.createElement("LABEL");
-    paska.append(text);
-    document.body.appendChild(paska);
+    if (text == 'Player1card') {
+        var paska = document.createElement("LABEL");
+        paska.append('Player 1 card');
+        paska.id = 'textPlayer1';
+        document.body.appendChild(paska);
+    }
+
+    if (text == 'Player1usedSword') {
+        var paska = document.createElement("LABEL");
+        paska.append('Player 1 used Sword');
+        paska.id = 'textSword1';
+        document.body.appendChild(paska);
+    }
+
+    if (text == 'Player2card') {
+        var paska = document.createElement("LABEL");
+        paska.append('Player 2 card');
+        paska.id = 'textPlayer2';
+        document.body.appendChild(paska);
+    }
+
+
+    var myImage = new Image(200, 400);
+    myImage.id = text;
 
     if (card == "zs1" || card == "zs2") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/zero_skull.jpg';
         document.body.appendChild(myImage);
     } 
 
     if (card == "z1" || card == "z2" || card == "z3" || card == "z4" || card == "z5"
         || card == "z6" || card == "z7" || card == "z8") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/zero.jpg';
         document.body.appendChild(myImage);
     } 
 
     if (card == "1s1" || card == "1s2" || card == "1s3" || card == "1s4") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/one_sword.jpg';
         document.body.appendChild(myImage);
     } 
 
     if (card == "1t1" || card == "1t2" || card == "1t3" || card == "1t4") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/one_tower.jpg';
         document.body.appendChild(myImage);
     } 
 
     if (card == "2.1" || card == "2.2" || card == "2.3" || card == "2.4") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/two.jpg';
         document.body.appendChild(myImage);
     } 
 
     if (card == "3.1" || card == "3.2") {
-        var myImage = new Image(200, 400);
         myImage.src = './resources/three.jpg';
         document.body.appendChild(myImage);
     }
+}
+
+function hideId(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+function showId(id) {
+    document.getElementById(id).style.display = 'inline-block';
+}
+
+function hideAll() {
+    var p1c = document.getElementById('Player1card');
+    var p2c = document.getElementById('Player2card');
+    var p3c = document.getElementById('Player1usedSword');
+    var p4c = document.getElementById('textPlayer1');
+    var p5c = document.getElementById('textSword1');
+    var p6c = document.getElementById('textPlayer2');
+
+    if (p1c != null) {
+        p1c.parentNode.removeChild(p1c);
+    }
+
+    if (p2c != null) {
+        p2c.parentNode.removeChild(p2c);
+    }
+
+    if (p3c != null) {
+        p3c.parentNode.removeChild(p3c);
+    }
+
+    if (p4c != null) {
+        p4c.parentNode.removeChild(p4c);
+    }
+
+    if (p5c != null) {
+        p5c.parentNode.removeChild(p5c);
+    }
+
+    if (p6c != null) {
+        p6c.parentNode.removeChild(p6c);
+    }
+
+   
 }
