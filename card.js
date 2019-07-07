f706773eaeb2efdaadb85e6a9cb2dc415fa07865
Aleksi <aleksi.martikainen@metropolia.fi>
2019-07-07 17:31:57 +0300

    code cleaning

--- card.js
+++ card.js
@@ -2,8 +2,6 @@
 
 var cards = [];
 
-var count = 0;
-
 function start() {
 
     cards = [];
@@ -22,14 +20,11 @@ function start() {
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
 
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
 
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
@@ -41,61 +36,54 @@ function shuffle(array) {
 
 function showCard(card, text, id) {
 
+    var paska = document.createElement("LABEL");
+
     if (text == 'Player1card') {
-        var paska = document.createElement("LABEL");
         paska.append('Player 1 card');
         paska.id = 'textPlayer1';
-        document.getElementById(id).appendChild(paska);
     }
 
     if (text == 'Player1usedSword') {
-        var paska = document.createElement("LABEL");
         paska.append('Player 1 used Sword');
         paska.id = 'textSword1';
-        document.getElementById(id).appendChild(paska);
     }
 
     if (text == 'Player2card') {
-        var paska = document.createElement("LABEL");
         paska.append('Player 2 card');
         paska.id = 'textPlayer2';
-        document.getElementById(id).appendChild(paska);
     }
 
+    document.getElementById(id).appendChild(paska);
 
     var myImage = new Image(200, 400);
     myImage.id = text;
 
     if (card == "zs1" || card == "zs2") {
         myImage.src = './resources/zero_skull.jpg';
-        document.getElementById(id).appendChild(myImage);
     } 
 
     if (card == "z1" || card == "z2" || card == "z3" || card == "z4" || card == "z5"
         || card == "z6" || card == "z7" || card == "z8") {
         myImage.src = './resources/zero.jpg';
-        document.getElementById(id).appendChild(myImage);
     } 
 
     if (card == "1s1" || card == "1s2" || card == "1s3" || card == "1s4") {
         myImage.src = './resources/one_sword.jpg';
-        document.getElementById(id).appendChild(myImage);
     } 
 
     if (card == "1t1" || card == "1t2" || card == "1t3" || card == "1t4") {
         myImage.src = './resources/one_tower.jpg';
-        document.getElementById(id).appendChild(myImage);
     } 
 
     if (card == "2.1" || card == "2.2" || card == "2.3" || card == "2.4") {
         myImage.src = './resources/two.jpg';
-        document.getElementById(id).appendChild(myImage);
     } 
 
     if (card == "3.1" || card == "3.2") {
         myImage.src = './resources/three.jpg';
-        document.getElementById(id).appendChild(myImage);
     }
+
+    document.getElementById(id).appendChild(myImage);
 }
 
 function hideId(id) {
@@ -106,37 +94,19 @@ function showId(id) {
     document.getElementById(id).style.display = 'inline-block';
 }
 
+function removeElement(id) {
+    var element = document.getElementById(id);
+
+    if (element != null) {
+        element.parentNode.removeChild(element);
+    }
+}
+
 function hideAll() {
-    var p1c = document.getElementById('Player1card');
-    var p2c = document.getElementById('Player2card');
-    var p3c = document.getElementById('Player1usedSword');
-    var p4c = document.getElementById('textPlayer1');
-    var p5c = document.getElementById('textSword1');
-    var p6c = document.getElementById('textPlayer2');
-
-    if (p1c != null) {
-        p1c.parentNode.removeChild(p1c);
-    }
-
-    if (p2c != null) {
-        p2c.parentNode.removeChild(p2c);
-    }
-
-    if (p3c != null) {
-        p3c.parentNode.removeChild(p3c);
-    }
-
-    if (p4c != null) {
-        p4c.parentNode.removeChild(p4c);
-    }
-
-    if (p5c != null) {
-        p5c.parentNode.removeChild(p5c);
-    }
-
-    if (p6c != null) {
-        p6c.parentNode.removeChild(p6c);
-    }
-
-   
+    removeElement('Player1card');
+    removeElement('Player2card');
+    removeElement('Player1usedSword');
+    removeElement('textPlayer1');
+    removeElement('textSword1');
+    removeElement('textPlayer2');   
 }
