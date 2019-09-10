8a79bdb5570fb3a8a30edc796ed1c3b2ab597810
Aleksi Martikainen <33517887+AleksiMartikainen@users.noreply.github.com>
2019-09-10 04:15:12 +0300

    simple stats

--- card.js
+++ card.js
@@ -2,6 +2,12 @@
 
 var cards = [];
 var turns = 0;
+var zeros = 0;
+var zerosS = 0;
+var oneS = 0;
+var oneT = 0;
+var two = 0;
+var three = 0;
 
 function start() {
 
@@ -38,51 +44,57 @@ function shuffle(array) {
 
 function showCard(card, text, id) {
 
-    var paska = document.createElement("LABEL");
+    var label = document.createElement("LABEL");
 
     if (text == 'Player1card') {
-        paska.append('Player 1 card');
-        paska.id = 'textPlayer1';
+        label.append('Player 1 card');
+        label.id = 'textPlayer1';
     }
 
     if (text == 'Player1usedSword') {
-        paska.append('Player 1 used Sword');
-        paska.id = 'textSword1';
+        label.append('Player 1 used Sword');
+        label.id = 'textSword1';
     }
 
     if (text == 'Player2card') {
-        paska.append('Player 2 card');
-        paska.id = 'textPlayer2';
+        label.append('Player 2 card');
+        label.id = 'textPlayer2';
     }
 
-    document.getElementById(id).appendChild(paska);
+    document.getElementById(id).appendChild(label);
 
     var myImage = new Image(200, 700);
     myImage.id = text;
 
     if (card == "zs1" || card == "zs2") {
         myImage.src = './resources/zero_skull.jpg';
+        zerosS++;
     } 
 
     if (card == "z1" || card == "z2" || card == "z3" || card == "z4" || card == "z5"
         || card == "z6" || card == "z7" || card == "z8") {
         myImage.src = './resources/zero.jpg';
+        zeros++;
     } 
 
     if (card == "1s1" || card == "1s2" || card == "1s3" || card == "1s4") {
         myImage.src = './resources/one_sword.jpg';
+        oneS++;
     } 
 
     if (card == "1t1" || card == "1t2" || card == "1t3" || card == "1t4") {
         myImage.src = './resources/one_tower.jpg';
+        oneT++;
     } 
 
     if (card == "2.1" || card == "2.2" || card == "2.3" || card == "2.4") {
         myImage.src = './resources/two.jpg';
+        two++;
     } 
 
     if (card == "3.1" || card == "3.2") {
         myImage.src = './resources/three.jpg';
+        three++;
     }
 
     document.getElementById(id).appendChild(myImage);
@@ -96,6 +108,14 @@ function hideId(id) {
 
 function showId(id) {
     document.getElementById(id).style.display = 'inline-block';
+}
+
+function disableId(id) {
+    document.getElementById(id).disabled = true;
+}
+
+function enableId(id) {
+    document.getElementById(id).disabled = false;
 }
 
 function removeElement(id) {
@@ -115,10 +135,29 @@ function hideAll() {
     removeElement('textPlayer2');   
 }
 
-function checkSkull(paska) {
+function checkSkull(card) {
     var audio = new Audio('./resources/mlg.mp3');
 
-    if (paska == 'zs1' || paska == 'zs2') {
+    if (card == 'zs1' || card == 'zs2') {
         audio.play();
     }
+}
+
+function showStats() {
+    removeElement('statsInfo');
+    var label = document.createElement("LABEL");
+    label.id = 'statsInfo';
+
+    label.append(
+        'Turns: ' + turns + 
+        ' | Zeros: ' + zeros + 
+        ' | Skulls: ' + zerosS + 
+        ' | Swords: ' + oneS + 
+        ' | Towers: ' + oneT + 
+        ' | Twos: ' + two +
+        ' | Threes: ' + three
+    );
+    
+
+    document.getElementById('stats').appendChild(label);
 }
